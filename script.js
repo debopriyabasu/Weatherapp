2dc798fddb260dd4837226c6e40ec2f283ed1700
debopriyabasu <85153924+debopriyabasu@users.noreply.github.com>
2021-06-27 17:59:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-s
+let weather= {
+    "apiKey": "a124262df13655cccf6be6b8317f3e59",
+    fetchWeather: function (city) {
+        fetch(
+            "http://api.openweathermap.org/data/2.5/weather?q=" 
+            + city
+             + "&units=metric&appid="
+             + this.apiKey
+            )
+            .then((response) => response.json())
+            .then((data) => console.log(data));  
+    },
+    displayWeather: function(data) {
+
+    }
+}
